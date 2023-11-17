150e0ba1bbe25e9a5c5b6fc9ca317e4eaf3d1dfa
Egor <des2107@mail.ru>
2023-11-17 14:05:20 +0300

    комит

---
+++ F1_pulse/F1_pulse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  F1_pulse
+//
+//  Created by Egor Diachkov on 17/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
